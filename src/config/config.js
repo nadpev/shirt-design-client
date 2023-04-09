167321863a941b74340705944702b862fb542745
nadpev <nadpev@gmail.com>
2023-04-09 19:41:23 +0300

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: 'https://shirt-design.onrender.com/api/v1/dalle',
+  },
+  production: {
+    backendUrl: 'https://shirt-design.onrender.com/api/v1/dalle',
+  },
+};
+
+export default config;
